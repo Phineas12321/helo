310723154a926418d8f65d142a880de43b3ba61d
George Andrew Phineas Coon <phineas@Georges-MacBook-Pro.local>
2020-02-24 12:18:36 -0700

    author id is now saved to the database

--- server/controller.js
+++ server/controller.js
@@ -12,8 +12,8 @@ module.exports = {
 
     addPost: (req, res) => {
         console.log(req)
-        const {author_id} = req.params
-        const {title, img, content} = req.body
+        //const {author_id} = req.params
+        const {author_id, title, img, content} = req.body
         const db = req.app.get('db')
         db.add_post([author_id, title, img, content]).then(()=>{
             res.sendStatus(201)
